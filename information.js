0311452917ccb6560205601a66a07689028819db
Sahrane_Guassemi <sahranedev@gmail.com>
2022-10-09 12:08:21 +0200

    MeuhMeuh Chal

---
+++ information.js
@@ -0,0 +1,13 @@
+const name = "Sahrane";
+const campus = "Lyon;"
+
+module.exports = {
+    name: name,
+    campus: campus,
+};
+
+const userInfo = require("./information");
+
+const cowsay = require("cowsay");
+
+
